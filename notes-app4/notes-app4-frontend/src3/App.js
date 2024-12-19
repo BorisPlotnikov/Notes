8342644567254c2408a16e63044cdb359c2d0c7a
Boris <boriswo@gmail.com>
2024-12-18 20:41:16 -0800

    Added addNote. Needs NoteForm next

--- notes-app4/notes-app4-frontend/src3/App.js
+++ notes-app4/notes-app4-frontend/src3/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import dotenv from 'dotenv';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import NoteList from './components/NoteList';
 import handleError from '../utils/errorHandler';
 import './css/App.css';
@@ -11,6 +12,22 @@ const App = () => {
     const editNote = async (id) => {};
     const deleteNote = async (id) => {};
     const [loading, setLoading] = useState(false);
+
+    const addNote = async (content, signal) => {
+        setLoading(true);
+        try {
+            const indexedNote = { id : uuidv4(), content : content };
+            const response = await axios.post(`${propcess.env.REACT_APP_API_BASE_URL}/notes`);
+            setNotes((prevNotes) => [...prevNotes, response.data]);
+        } catch (err) {
+            handleError(
+                setErrorMessage,
+                err
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
 
         return (
         <div className='app'>
