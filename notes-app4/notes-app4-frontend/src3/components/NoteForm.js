b52bc5fbe835f3df6996f3b4cbfc0a21d0a3bb2a
Boris <boriswo@gmail.com>
2024-12-24 20:31:50 -0800

    Enhansed validation for NoteForm

--- notes-app4/notes-app4-frontend/src3/components/NoteForm.js
+++ notes-app4/notes-app4-frontend/src3/components/NoteForm.js
@@ -6,21 +6,34 @@ import PropTypes from 'prop-types';
 const NoteForm = ({ addNote, setMessage, loading }) => {
 
     const [content, setContent] = useState('');
+    
+    const MAX_LENGTH = 200;
+    const MIN_LENGTH = 1;
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!content.trim()) {
-            handleError(setMessage, 'Empty content submission attempt');
+        const trimmedContent = content.trim();
+        if (!trimmedContent) {
+            handleError(setMessage, 'An attempt to submit empty content');
             return;
         }
+
+
+        if (trimmedContent.length < MIN_LENGTH || trimmedContent.length > MAX_LENGTH) {
+            const message = trimmedContent.length < MIN_LENGTH
+                ? `An attempt to submit less than ${MIN_LENGTH} characters.`
+                : `An attempt to submit more than ${MAX_LENGTH} characters.`;
+            handleError(setMessage, message);
+            return;
+        }        
+
         try {
-            await addNote(content);
+            await addNote(trimmedContent);
             setContent('');
         } catch (err) {
             handleError(setMessage, 'Saving failed', err);
         };
     };
-
 
     return (
         <form onSubmit={handleSubmit} className='note-form'>
@@ -29,12 +42,17 @@ const NoteForm = ({ addNote, setMessage, loading }) => {
             type='text'
             value={content}
             onChange={(e) => {
-                setContent(e.target.value);
+                setContent(e.target.value.trimStart().trimEnd());
             }}
             placeholder='Add a new note'
             aria-label='Note Content'
+            maxLength={MAX_LENGTH}
             />
-            <button type='submit' disabled={!content || loading}>{loading ? 'Adding...' : 'Add'}</button>
+            <div className={`character-counter ${content.length >= MAX_LENGTH - 20 ? 'warning' : ''}`} aria-live="polite">
+                {content.length} / {MAX_LENGTH}
+            </div>
+
+            <button type='submit' disabled={loading || !content}>{loading ? 'Adding...' : 'Add'}</button>
         </form>
     );
 };
