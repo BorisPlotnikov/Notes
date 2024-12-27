e061f48aedd06102d78ea96cea47e6d1398b61f0
Boris <boriswo@gmail.com>
2024-12-27 13:37:50 -0800

    Fixed abort controller logic in src2

--- notes-app4/notes-app4-frontend/src2/App.js
+++ notes-app4/notes-app4-frontend/src2/App.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import dotenv from 'dotenv';
 import axios from 'axios';
-import NoteList from './components/NoteList';
 import handleError from '../utils/errorHandler';
 import './css/App.css';
 dotenv.config();
 
 const App = () => {
     const [notes, setNotes] = useState([]);
-    const editNote = async (id) => {};
-    const deleteNote = async (id) => {};
+    const [errorMessage, setErrorMessage] = useState(null);
     const [loading, setLoading] = useState(false);
-
+   
+    const controller = new AbortController();
+    useEffect(() => { return () => controller.abort() }, []);
+    
     useEffect(() => {
-        const controller = new AbortController();
-
         const fetchNotes = async () => {
             setLoading(true);
             try {
@@ -39,21 +38,13 @@ const App = () => {
             }
         };
         fetchNotes();
-
-        return () => {
-            controller.abort();
-        };
     }, []);
 
         return (
         <div className='app'>
             <h1>Notes</h1>
-            <NoteList
-                notes={notes}
-                deleteNote={deleteNote}
-                editNote={editNote}
-            />         
         </div>
     );
 };
+
 export default App;
