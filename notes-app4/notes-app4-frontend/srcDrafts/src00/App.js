e240ea4d6de57e757ed9ce2095e5e5325acc73c1
BorisPlotnikov <boriswo@gmail.com>
2025-03-04 11:38:32 -0800

    Clean up and moving drafts to srcDrafts

---
+++ notes-app4/notes-app4-frontend/srcDrafts/src00/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './css/App.css';
+
+const App = () => {
+
+    return (
+        <div className='app'>
+            <h1>Notes</h1>           
+        </div>
+    );
+};
+export default App;
